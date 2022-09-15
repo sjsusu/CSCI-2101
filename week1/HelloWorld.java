a38896e90c18d414ed66c44c1b3d9226e87d982d
sjsusu <sus@go.stockton.edu>
2022-09-15 19:11:43 -0400

    Hello World

---
+++ week1/HelloWorld.java
@@ -0,0 +1,11 @@
+package week1;
+
+public class HelloWorld{
+    public static void main(String[] args) {
+        
+        System.out.println("hello world!");
+
+        System.out.println("\nhehe >:3");
+
+    }
+}
